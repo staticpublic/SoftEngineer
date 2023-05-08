08d35385059bdafc13a26959105003edacd3fcf3
staticpublic <2118738239@qq.com>
2023-05-08 15:22:50 +0800

    frame1.1

--- src/main/java/controller/testController.java
+++ src/main/java/controller/testController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/home")
 public class testController {
-    @RequestMapping("/test")
+    @RequestMapping("/tes")
     public String index(){
-        return "test";
+        return "tes";
     }
 }
